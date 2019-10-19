f45ca353c0ce62e077bf9db41c9c3e8970ba6e9a
Cla Ra <clara79dev@gmail.com>
2019-10-20 00:36:42 +0200

    global code revision state based

--- bidoo_hacking.js
+++ bidoo_hacking.js
@@ -1,45 +1,137 @@
-function bidSetup(userName, auctionId, maxPurchasePricePercentage=50, decimalSign=",") {
-	if(typeof currentBid !== 'undefined')
-        clearInterval(currentBid);
+LOOPING = 0;
+UNKNOWN = 1;
+CANDIDATE_FOR_WINNING = 2;
+AUCTION_FINISHED = 3;
+COMING_SOON = 4;
 
+function bidSetupAndStart(userName, auctionId, maxPurchasePricePercentage=50, decimalSign=",") {
+	if(typeof attemptTask !== 'undefined')
+        clearTimeout(attemptTask);
+
+    bidSetup(decimalSign, auctionId, maxPurchasePricePercentage, userName);
+
+    var currentTimeText = timeElement.text();
+    var waitingDelay = calculateDelay(currentTimeText);
+
+    attemptTask = setTimeout(bid, waitingDelay);
+}
+
+function bidSetup(decimalSign, auctionId, maxPurchasePricePercentage, userName) {
+    startDate = new Date();
+    clickCounter = 0;
     currentRegExp = decimalSign === "," ? /[^0-9\,-]+/g : /[^0-9\.-]+/g;
     auctionElement = $("#divAsta" + auctionId);
-	timeElement = $("#TempoMancante" + auctionId);
+    timeElement = $("#TempoMancante" + auctionId);
     linkElement = $("#mehodkbdkbd" + auctionId)[0];
-    linkElementText = $("#mehodkbdkbd" + auctionId).text();
+    linkElementForText = $("#mehodkbdkbd" + auctionId);
+    linkElementText = linkElementForText.text();
     currentPriceElement = $("#PrezzoAsta" + auctionId);
     normalPriceElement = $("#divAsta" + auctionId + " .buy-rapid-now");
     normalPrice = currency2Number(normalPriceElement.text().trim());
     priceThreshold = normalPrice * maxPurchasePricePercentage / 100;
     winnerElement = $("#Vincitore" + auctionId);
     loggedUserName = userName;
-
+    justChecked = false;
+    currentTimeText = null;
+    currentWinnerName = null;
+    currentPrice = Number.MAX_VALUE;
     bidHackData = {
         who: loggedUserName,
         what: auctionId,
         threshold: priceThreshold
     };
-
     console.log(bidHackData);
-
-	puntata = setInterval(punta, 100);
 }
 
-var punta = function () {
-    var timeText = timeElement.text();
-    var winnerName = winnerElement.text();
-    var curentPriceText = currentPriceElement.text().trim();
-    var curentPrice = currency2Number(curentPriceText);
+var bid = function () {
+    updateInputs();
 
-    console.log(timeText, curentPrice, priceThreshold);
+    var curentState = calculateState();
 
-	if(timeText === "00:00" && winnerName != loggedUserName && curentPrice <= priceThreshold) {
-		linkElement.click();
-		clearInterval(puntata);
-	} else if(linkElementText === "VENDUTO" || curentPrice > priceThreshold) {
-        clearInterval(puntata);
+    switch (curentState) {
+        case CANDIDATE_FOR_WINNING:
+            tryToClick();
+            break;
+        case AUCTION_FINISHED:
+            onFinish();
+            break;
+        case COMING_SOON:
+        default:
+            goToSleep();
     }
+
+    // if(currentTimeText === "00:00"
+    //     && currentWinnerName != loggedUserName
+    //     && currentPrice <= priceThreshold) {
+    //         tryToClick();
+    // } else if(linkElementText === "VENDUTO"
+    //     || currentTimeText === "Controllo..."
+    //     || currentTimeText === "Ha Vinto!"
+    //     || currentTimeText === "Hai Vinto!"
+    //     || currentPrice > priceThreshold) {
+    //     onFinish();
+    // } else {
+    //     goToSleep();
+    // }
 };
+
+function onFinish() {
+    stopDate = new Date();
+    console.log("Time consumed: " + stopDate - startDate);
+    console.log("Bid consumed: " + clickCounter);
+}
+
+function tryToClick() {
+    if (justChecked) {
+        // linkElement.click();
+        console.log("CLICK");
+        ++clickCounter;
+        attemptTask = setTimeout(bid, 2000);
+    }
+    else {
+        justChecked = true;
+        attemptTask = setTimeout(bid, 1000);
+    }
+}
+
+function goToSleep() {
+    justChecked = false;
+    var waitingDelay = calculateDelay(currentTimeText);
+    if (waitingDelay > 0)
+        attemptTask = setTimeout(bid, waitingDelay);
+    else
+        attemptTask = setTimeout(bid, 250);
+}
+
+function updateInputs() {
+    currentTimeText = timeElement.text();
+    currentWinnerName = winnerElement.text();
+    var currentPriceText = currentPriceElement.text().trim();
+    currentPrice = currency2Number(currentPriceText);
+    linkElementText = linkElementForText.text();
+    console.log(currentTimeText, currentWinnerName, currentPrice, priceThreshold);
+}
+
+function calculateState() {
+    if(currentTimeText === "00:00"
+        && currentWinnerName != loggedUserName
+        && currentPrice <= priceThreshold)
+        return CANDIDATE_FOR_WINNING;
+
+    if(linkElementText === "VENDUTO"
+    || currentTimeText === "Ha Vinto!"
+    || currentTimeText === "Hai Vinto!"
+    || currentPrice > priceThreshold)
+        return AUCTION_FINISHED;
+
+    if(currentTimeText === "Controllo...")
+        return UNKNOWN;
+
+    if(linkElementText === "RIAPRE PRESTO")
+        return COMING_SOON;
+
+    return LOOPING;
+}
 
 function currency2Number(currencyText) {
     var cleanCurrencyText = currencyText.replace(currentRegExp, "");
@@ -47,5 +139,13 @@ function currency2Number(currencyText) {
     return Number(cleanCurrencyText);
 }
 
-
+function calculateDelay(timeText) {
+    var [minutesText, secondsText] = timeText.split(":");
+    var minutes = Number(minutesText);
+    var seconds = Number(secondsText);
+    var delay = (minutes * 60 + seconds) * 1000;
+    delay = isNaN(delay) ? 60000 : delay;
+    console.log("Delay: " + delay);
+    return delay;
+}
 
